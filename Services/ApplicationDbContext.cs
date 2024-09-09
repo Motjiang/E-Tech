65f339bdbf73666b5b1e4249d723668556302d0b
Motjiang <motjiangbrilliant@gmail.com>
2024-09-09 03:50:59 +0200

    Registered the DbContext

---
+++ Services/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace E_Tech.Services
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        //public DbSet<Product> Products { get; set; }
+        //public DbSet<Category> Categories { get; set; }
+        //public DbSet<Brand> Brands { get; set; }
+        //public DbSet<Cart> Carts { get; set; }
+        //public DbSet<CartItem> CartItems { get; set; }
+        //public DbSet<Order> Orders { get; set; }
+        //public DbSet<OrderItem> OrderItems { get; set; }
+    }
+}
