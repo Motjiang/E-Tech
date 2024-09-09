63f92186e57395f1f47a2e3779529b009a2075f3
Motjiang <motjiangbrilliant@gmail.com>
2024-09-09 03:57:56 +0200

    Annotated the Product model

---
+++ Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace E_Tech.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string Name { get; set; } = "";
+        [Required]
+        [MaxLength(100)]
+        public string Brand { get; set; } = "";
+        [Required]
+        [MaxLength(100)]
+        public string Category { get; set; } = "";
+        [Required]
+        [Precision(16, 2)]
+        public decimal Price { get; set; }
+        [Required]
+        public string Description { get; set; } = "";
+        [Required]
+        [MaxLength(100)]
+        public string ImageFile { get; set; } = "";
+        public DateTime CreatedAt { get; set; }
+    }
+}
