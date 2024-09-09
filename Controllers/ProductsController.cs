90db0df036a95352227bba3a26e3f8c43e2e21fd
Motjiang <motjiangbrilliant@gmail.com>
2024-09-09 04:23:39 +0200

    added the list product page

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using E_Tech.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Tech.Controllers
+{
+    public class ProductsController : Controller
+    {
+        //applicationDbContext
+        private readonly ApplicationDbContext _context;
+        //constructor
+        public ProductsController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var products = _context.Products.ToList();
+            return View(products);
+        }
+    }
+}
