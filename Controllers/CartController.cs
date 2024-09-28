5c40af83eea620b442fc9c9825c7e5bdb31fc7f0
Motjiang <motjiangbrilliant@gmail.com>
2024-09-28 22:29:09 +0200

    Added an Index method to View the Items

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -1,11 +1,37 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using E_Tech.Models;
+using E_Tech.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 
 namespace E_Tech.Controllers
 {
     public class CartController : Controller
     {
+
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly decimal shippingFee;
+
+        // constructor
+        public CartController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, IConfiguration configuration)
+        {
+            _context = context;
+            _userManager = userManager;
+            shippingFee = configuration.GetValue<decimal>("CartSettings:ShippingFee");
+        }
+
+
         public IActionResult Index()
         {
+            List<OrderItem> cartItems = CartHelper.GetCartItems(Request, Response, _context);
+            decimal subtotal = CartHelper.GetSubtotal(cartItems);
+
+
+            ViewBag.CartItems = cartItems;
+            ViewBag.ShippingFee = shippingFee;
+            ViewBag.Subtotal = subtotal;
+            ViewBag.Total = subtotal + shippingFee;
+
             return View();
         }
     }
