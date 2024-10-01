7aa2c642e9b535926ad62b7786121a4210003bed
Motjiang <motjiangbrilliant@gmail.com>
2024-10-02 00:26:40 +0200

    Added an index to view the check out

---
+++ Controllers/CheckoutController.cs
@@ -0,0 +1,34 @@
+﻿using E_Tech.Models;
+using E_Tech.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace E_Tech.Controllers
+{
+    public class CheckoutController : Controller
+    {
+        private readonly decimal _shippingFee;
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public CheckoutController(IConfiguration configuration, ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+
+            _shippingFee = configuration.GetValue<decimal>("CartSettings:ShippingFee");
+            _context = context;
+            _userManager = userManager;
+        }
+        public IActionResult Index()
+        {
+            List<OrderItem> cartItems = CartHelper.GetCartItems(Request, Response, _context);
+            decimal total = CartHelper.GetSubtotal(cartItems) + _shippingFee;
+
+            string deliveryAddress = TempData["DeliveryAddress"] as string ?? "";
+            TempData.Keep();
+
+            ViewBag.DeliveryAddress = deliveryAddress;
+            ViewBag.Total = total;
+            return View();
+        }
+    }
+}
